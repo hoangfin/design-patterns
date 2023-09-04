3f3fb3dc30f0431452ce115cd23f16dbdde877fe
Hoang Tran <hoang.tran.fin@gmail.com>
2023-09-04 23:40:30 +0300

    Observable class initial draft

---
+++ observer/js/observable.js
@@ -0,0 +1,32 @@
+class Observable {
+
+    constructor (subject) {
+        this.subject = subject;
+        this.observers = [];
+    }
+
+    subscribe(cb) {
+        this.observers.push(cb);
+    }
+
+    update(fn) {
+        this.subject = fn(this.subject);
+        this.notify();
+    }
+
+    notify() {
+        this.observers.forEach(observer => observer(this.subject));
+    }
+}
+
+const record = {
+    uuid: crypto.randomUUID(),
+    title: "UFO 1982 Research",
+    created: Date.now()
+};
+
+const obsRecord = new Observable(record);
+obsRecord.subscribe(record => console.log(record));
+obsRecord.update(record => {
+    record.title = "UFO 1982 Research - CANCELLED"
+});
